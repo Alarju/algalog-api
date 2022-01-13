c58fdfd60ac1965b6e22fff1352457fd8de50b32
Alarju <alex.geass@gmail.com>
2022-01-13 14:02:45 -0300

    Implementando JPA

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,31 +1,25 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
+	private ClienteRepository repository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var Cliente1 = new Cliente();
-		Cliente1.setId(1L);
-		Cliente1.setNome("João");
-		Cliente1.setTelefone("11 989206528");
-		Cliente1.setEmail("joao.ramalho@gmail.com");
-		
-		var Cliente2 = new Cliente();
-		Cliente2.setId(2L);
-		Cliente2.setNome("Maria");
-		Cliente2.setTelefone("11 963068287");
-		Cliente2.setEmail("maria.das.dores@gmail.com");
-		
-		return Arrays.asList(Cliente1, Cliente2);
+		return repository.findAll();
 		
 	}
 
